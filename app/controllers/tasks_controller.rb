3b6f45f2a29bb09132acb12d77a2076c89f67fd1
takashi.ogawa <ogawa.takashi@syuhari-tokyo.work>
2018-02-21 00:19:28 +0000

    added status column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -54,7 +54,7 @@ private
 
 #strong parameter
 def task_params
-  params.require(:task).permit(:content)
+  params.require(:task).permit(:content, :status)
 end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220122856) do
-
-  create_table "tasklists", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20180220235923) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
 end
